ebec6998f30ec15eb6943d500d9f5e9ea165aaed
jsbeckwith <lee.beckw@gmail.com>
2022-01-06 20:57:29 -0500

    files

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # strangeattrbot
+
+little [twitter bot](https://twitter.com/strangeattrbot) sporadically posting strange attractors
+
+method from [Sprott (1993)](https://sprott.physics.wisc.edu/pubs/PAPER203.HTM)
+
+requirements at [strangeattrbot-reqs.yml](/strangeattrbot-reqs.yml)
+
+a few code snippets from [here](https://github.com/icecolbeveridge/strangeAttractors)
+
+### more details
+
+this code iteratively generates points using the follow quadratic equations:
+x<sub>n+1</sub> = a<sub>1</sub> + a<sub>2</sub>x<sub>n</sub> + a<sub>3</sub>x<sub>n</sub><sup>2</sup> + a<sub>4</sub>x<sub>n</sub>y<sub>n</sub> + a<sub>5</sub>y<sub>n</sub> + a<sub>6</sub>y<sub>n</sub><sup>2</sup>
+y<sub>n+1</sub> = a<sub>7</sub> + a<sub>8</sub>y<sub>n</sub> + a<sub>9</sub>y<sub>n</sub><sup>2</sup> + a<sub>10</sub>y<sub>n</sub>x<sub>n</sub> + a<sub>11</sub>x<sub>n</sub> + a<sub>12</sub>x<sub>n</sub><sup>2</sup>
+
+the initial point is (0.05, 0.05)
+
+the coefficients are randomly chosen in increments of 0.1 in the range [-1.2, 1.2]
+
+each value is assigned a letter A->Y; for example -1.2 = A; -1.1 = B ... 1.1 = X; 1.2 = Y
+
+thus, each image's associated 12-letter string can be translated into its associated coefficients
+
+### sample images
+
+[](/swallow.png)
+AGVTEVQRUITD
+
+[](/tri.png)
+VKKUKGGBOLLN
---
+++ str_attr.py
@@ -0,0 +1,162 @@
+import random
+import matplotlib.pyplot as plt
+import tweepy
+import os
+from math import log2, sqrt
+from dotenv import load_dotenv
+
+# load constants from .env file
+load_dotenv()
+
+# twitter api keys
+CONSUMER_KEY = os.getenv("CONSUMER_KEY")
+CONSUMER_SECRET = os.getenv("CONSUMER_SECRET")
+KEY = os.getenv("KEY")
+SECRET = os.getenv("SECRET")
+
+# absolute path for cron
+PATH = os.getenv("ABS_PATH")
+
+# lots of this very beautiful & clean code is adapted from this repository:
+# https://github.com/icecolbeveridge/strangeAttractors
+LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXY"
+
+generateAttrString = lambda n: "".join( [random.choice(LETTERS) for i in range(n)])
+
+letterToNumber = lambda x: 0.1*(LETTERS.index(x) - 12)
+
+# get the next value in the series
+def nextVal( x, y, params ):
+    a,b,c,d,e,f = params
+    return a + b*x + c*x*x + d*x*y + e*y + f*y*y
+
+# generate a chaotic set of coefficients
+def generateAttractor(init = (0.05,0.05) ):
+    attrString = generateAttrString(12)
+    
+    print(attrString)
+    xparams = list(map(letterToNumber, attrString[:6]))
+    yparams = list(map(letterToNumber, attrString[6:]))
+
+    x,y = init
+
+    # initial iteration to check for divergence to infinity
+    for i in range(100):
+        x,y = nextVal(x,y,xparams), nextVal(x,y,yparams)
+        
+        # check for divergence to infinity
+        if abs(x) > 1.e5:
+            return None
+
+    lyapunov = 0
+    
+    x1 = x
+    y1 = y
+    x2 = x + 10e-6
+    y2 = y
+
+    # calculate lyapunov exponent after 11000 iterations
+    for j in range(1,11000):
+        results = calculateLyapunov(x1, y1, x2, y2, xparams, yparams)
+        to_add = results[0]
+        x1 = results[1]
+        y1 = results[2]
+        x2 = results[3]
+        y2 = results[4]
+        lyapunov = (lyapunov + to_add)
+    
+    lyapunov = 1/j * lyapunov
+    
+    # check for chaotic behavior
+    if lyapunov > 0.001:
+        print(lyapunov)
+        return attrString
+    else:
+        return None
+        
+# helper fn to calculate euclidian distance
+def calculateDistance(x1, y1, x2, y2):
+    xdist_n = abs(x2 - x1)
+    ydist_n = abs(y2 - y1)
+    return sqrt(xdist_n**2 + ydist_n**2)
+
+# calculate a term of the sum for the lyapunov exponent
+def calculateLyapunov(x1, y1, x2, y2, xparams, yparams):
+    dn = calculateDistance(x1, y1, x2, y2)
+    x1new = nextVal(x1, y1, xparams)
+    y1new = nextVal(x1, y1, yparams)
+    x2new = nextVal(x2, y2, xparams)
+    y2new = nextVal(x2, y2, yparams)
+    dn1 = calculateDistance(x1new, y1new, x2new, y2new)
+    if dn == 0 or dn1/dn == 0:
+        log_to_add = 0
+    else:
+        log_to_add = log2(dn1/dn)
+    return [log_to_add, x1new, y1new, x2new, y2new]
+
+# given a string of coefficients, generate a certain number
+# of points in the series to eventually graph
+def generatePoints(s, num_iter=100000, init=(0.05, 0.05)):
+    xparams = list(map(letterToNumber, s[:6]))
+    yparams = list(map(letterToNumber, s[6:]))
+    
+    out = [init]
+    x,y = init
+    
+    for k in range(num_iter):
+        x,y = nextVal(x,y,xparams), nextVal(x,y,yparams)
+        out.append((x,y))
+    
+    return out
+
+# create and save a plot
+def plot(points, s, ms = 0.05):
+    metadata = {"Title": s}
+    x,y = [p[0] for p in points], [p[1] for p in points]
+    fig, ax = plt.subplots()
+    ax.plot(x,y,'k+', markersize = ms)
+    ax.set_axis_off()
+    fig.savefig(PATH, dpi=300, metadata=metadata)
+
+# if string is given, save plot of that string
+# otherwise find a chaotic string and save plot
+def go(s = None):
+    while s == None:
+        s = generateAttractor()
+        
+    out = generatePoints(s)
+    
+    plot(out, s)
+
+    return s
+
+# tweeting infrastructure
+def main():
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(KEY, SECRET)
+
+    api = tweepy.API(auth)
+    
+    try:
+        api.verify_credentials()
+        print("Authentication OK")
+    except:
+        print("Error during authentication")
+
+    try:
+        tweet = go()
+        media = api.media_upload(PATH)
+        api.update_status(status=tweet, media_ids=[media.media_id])
+        print("tweeted", tweet)
+    except:
+        pass
+
+main()
+
+# cool attractors ;)
+# VGDJVHLHQKDP
+# GAUVHRWSVXEO
+# PLQFERMYONKM
+# NEKBSTRRCHPP
+# FPOUVQXOITLA
+# AGVTEVQRUITD
