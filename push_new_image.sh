a6319e03e91c2c0af2708b7b5a741f5d7f83aae6
Lee Beckwith <lbeckwith@lbeckwith.local>
2023-12-16 13:13:36 -0500

    new image

---
+++ push_new_image.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+python3 gen_img_for_gh.py
+
+# Navigate to the directory of your Git repository
+cd ~/lbeckwith/strangeattrbot
+
+# Add all changes to the staging area
+git add .
+
+# Commit changes with a message
+git commit -m "new image"
+
+# Push changes to the default branch (e.g., master)
+git push
